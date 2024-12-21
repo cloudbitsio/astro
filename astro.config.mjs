1e81957038142fac54367fad88611ad70239fd93
Saddam Azad <saddam.azad@me.com>
2024-12-22 02:01:59 +0600

    astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'astro/config';
+// @ts-check
+import { defineConfig, envField } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	env: {
+		schema: {
+			PUBLIC_COLOR: envField.string({ context: "client", access: "public", optional: false }),
+		}
+	},
 	vite: {
 		build: {
 			exclude: ['stack/*']
